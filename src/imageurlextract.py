40c5a6a7cfde3d382d1f687ab21a6648fca0404c
5tuartw <stuwilson@gmail.com>
2025-01-20 12:36:32 +0000

    First draft image url extractions

--- src/imageurlextract.py
+++ src/imageurlextract.py
@@ -2,6 +2,10 @@ import re
 
 def extract_markdown_images(text):
   # r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+  matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+  return matches
 
 def extract_markdown_url(text):
   # r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+  matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+  return matches
