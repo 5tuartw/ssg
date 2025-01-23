c1fb11b53701517495e7c32106fc0bf4b1794c4e
stuartw <stuwilson@gmail.com>
2025-01-23 21:25:03 +0000

    finished inline markup node splitting phew!

--- src/main.py
+++ src/main.py
@@ -104,11 +104,11 @@ def split_nodes_image(old_nodes):
             image_url = extracted_image[1]
             this_split = old_node_text.split(f"![{alt_text}]({image_url})",1)
             if this_split[0] != "":
-                new_nodes.append(TextNode(this_split[0], TextType.TEXT))
+                new_nodes.append(TextNode(this_split[0], old_node.text_type))
             new_nodes.append(TextNode(alt_text, TextType.IMAGE, image_url))
             old_node_text = this_split[1]
         if old_node_text != "":
-            new_nodes.append(TextNode(old_node_text, TextType.TEXT))
+            new_nodes.append(TextNode(old_node_text, old_node.text_type))
     
     return new_nodes
 
@@ -116,23 +116,48 @@ def split_nodes_link(old_nodes):
     new_nodes = []
 
     for old_node in old_nodes:
-        old_node_text = old_node.text
-        #print(f"The old text is: {old_node_text}")
-        extracted_links = extract_markdown_links(old_node_text)
-        #print(f"Links extracted: {extracted_links}")
-        for extracted_link in extracted_links:
-            link_text = extracted_link[0]
-            link_url = extracted_link[1]
-            this_split = old_node_text.split(f"[{link_text}]({link_url})",1)
-            #print(f"The current splits are: {this_split}")
-            if this_split[0] != "":
-                new_nodes.append(TextNode(this_split[0], TextType.TEXT))
-            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
-            old_node_text = this_split[1]
-        if old_node_text != "":
-            new_nodes.append(TextNode(old_node_text, TextType.TEXT))
+        if old_node.text_type == TextType.IMAGE:
+            new_nodes.append(old_node)
+        else:
+            old_node_text = old_node.text
+            #print(f"The old text is: {old_node_text}")
+            extracted_links = extract_markdown_links(old_node_text)
+            #print(f"Links extracted: {extracted_links}")
+            for extracted_link in extracted_links:
+                link_text = extracted_link[0]
+                link_url = extracted_link[1]
+                this_split = old_node_text.split(f"[{link_text}]({link_url})",1)
+                #print(f"The current splits are: {this_split}")
+                if this_split[0] != "":
+                    new_nodes.append(TextNode(this_split[0], old_node.text_type))
+                new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+                old_node_text = this_split[1]
+            if old_node_text != "":
+                new_nodes.append(TextNode(old_node_text, old_node.text_type))
     
     return new_nodes
+
+def text_to_textnodes(text):
+    split_images = split_nodes_image([TextNode(text, TextType.TEXT)])
+    #print(len(split_images))
+    #print(split_images)
+    split_images_and_delimiters = []
+    list_of_delimiters = [("**", TextType.BOLD), ("*", TextType.ITALIC),("`", TextType.CODE)]
+    for node in split_images:
+        #print(node)
+        processed_nodes = [node]
+        for delimiter, text_type in list_of_delimiters:
+            #print(f"Processing delimiter: {delimiter}")
+            #print(f"Current processed_nodes: {processed_nodes}")
+            if node.text_type != TextType.IMAGE:
+                processed_nodes = split_nodes_delimiter(processed_nodes, delimiter, text_type)
+        split_images_and_delimiters.extend(processed_nodes)
+    #print(f"After delimiter work: {split_images_and_delimiters}")    
+
+    all_splits = split_nodes_link(split_images_and_delimiters)
+    #print(f"After splitting links: {all_splits}")
+    return all_splits
+            
 
 if __name__ == "__main__":
     main()
---
+++ src/test_text_to_textnode.py
@@ -0,0 +1,56 @@
+import unittest
+
+from main import *
+
+class testsplitnodelink(unittest.TestCase):
+    def test_split_single_link(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        text_nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(text_nodes, expected)
+
+    def test_simple_text(self):
+        text = "Hello world"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 1)
+        self.assertEqual(nodes[0].text, "Hello world")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+    
+    def test_bold_text(self):
+        text = "Hello **world**"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(len(nodes), 2)
+        self.assertEqual(nodes[0].text, "Hello ")
+        self.assertEqual(nodes[1].text, "world")
+        self.assertEqual(nodes[1].text_type, TextType.BOLD)
+    
+    def test_sequential_formatting(self):
+        text = "This is **bold** and *italic* text"
+        nodes = text_to_textnodes(text)
+        
+        self.assertEqual(len(nodes), 5)
+        self.assertEqual(nodes[0].text, "This is ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        
+        self.assertEqual(nodes[1].text, "bold")
+        self.assertEqual(nodes[1].text_type, TextType.BOLD)
+        
+        self.assertEqual(nodes[2].text, " and ")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+        
+        self.assertEqual(nodes[3].text, "italic")
+        self.assertEqual(nodes[3].text_type, TextType.ITALIC)
+        
+        self.assertEqual(nodes[4].text, " text")
+        self.assertEqual(nodes[4].text_type, TextType.TEXT)
