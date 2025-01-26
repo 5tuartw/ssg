8c9d613d1ffe60093f7eb9bdfefee3b09e90346c
stuartw <stuwilson@gmail.com>
2025-01-26 09:33:14 +0000

    altered gitignore

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ from imglinkextract import *
 from node_parser import *
 from html_converter import *
 from markdown_converter import *
+import os
+
+if os.path.exists("../static"):
+    print("The 'static' directly exists!")
+else:
+    print("No sign of the 'static' dir here.")
+
 
 def main():
     test = TextNode("Testing", TextType.BOLD, "http://yadayada")
